5b35ec8636fcdbf4392fc676dda2d047efb51133
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-03 15:33:05 -0300

    initial setup

---
+++ server/src/test/java/com/victorpereira/financialcontrol/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorpereira.financialcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
