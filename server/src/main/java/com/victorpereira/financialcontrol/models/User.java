b112bb3c7d3a6051f6e9fc132dc73c6d081237d2
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-03 17:54:14 -0300

    feat: user REST endpoints

--- server/src/main/java/com/victorpereira/financialcontrol/models/User.java
+++ server/src/main/java/com/victorpereira/financialcontrol/models/User.java
@@ -2,7 +2,6 @@ package com.victorpereira.financialcontrol.models;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
---
+++ server/src/main/java/com/victorpereira/financialcontrol/resources/UserResource.java
@@ -0,0 +1,56 @@
+package com.victorpereira.financialcontrol.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.victorpereira.financialcontrol.models.User;
+import com.victorpereira.financialcontrol.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@GetMapping
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+	
+	@GetMapping(value="/{id}")
+	public User findById(@PathVariable Integer id) {
+		return repo.findById(id).orElseThrow();
+	}
+
+	@PostMapping
+	public User insert(@RequestBody User user) {
+		return repo.save(user);
+	}
+	
+	@PutMapping(value="/{id}")
+	public User update(@RequestBody User user, @PathVariable Integer id) {
+		User usr = findById(id);
+		usr.setBalance(user.getBalance());
+		usr.setRevenue(user.getRevenue());
+		usr.setExpenses(user.getExpenses());
+		return repo.save(usr);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Integer id) {
+		repo.delete(findById(id));
+	}
+}
