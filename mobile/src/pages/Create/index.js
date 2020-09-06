9f46b2fd5d2300ed55ffb20fe8620dc7b137ded9
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-06 03:45:25 -0300

    style: create page layout

--- mobile/src/pages/Create/index.js
+++ mobile/src/pages/Create/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
+import { RectButton, TextInput } from 'react-native-gesture-handler';
 
 import api from '../../services/api';
 
@@ -8,9 +8,82 @@ export default class Create extends Component {
 
     render() {
         return (
-            <View>
-                <Text>CREATE PAGE</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>Add a Transaction</Text>
+
+                <View style={styles.inputContainer}>
+                    <Text style={styles.placeholder}>Name</Text>
+                    <TextInput style={styles.input} />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.placeholder}>Value</Text>
+                    <TextInput keyboardType='numeric' style={styles.input} />
+                </View>
+
+                <RectButton style={styles.confirmButton}>
+                    <Text style={styles.buttonText}>Confirm</Text>
+                </RectButton>
+                <RectButton style={styles.cancelButton} onPress={() => {
+                    this.props.navigation.navigate('Main')
+                }}>
+                    <Text style={styles.buttonText}>Cancel</Text>
+                </RectButton>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginRight: 15,
+        marginLeft: 15,
+        backgroundColor: '#f0f0f0'
+    },
+    title: {
+        color: '#616161',
+        marginTop: 20,
+        paddingBottom: 10,
+        fontSize: 20,
+        fontFamily: 'Ubuntu_400Regular',
+        borderBottomWidth: 1.5,
+        borderBottomColor: '#bfbfbf',
+    },
+    inputContainer: {
+        marginTop: 30,
+    },
+    placeholder: {
+        color: '#616161',
+        fontSize: 17,
+        fontFamily: 'Ubuntu_400Regular',
+        paddingBottom: 5
+    },
+    input: {
+        fontSize: 17,
+        fontFamily: 'Ubuntu_400Regular',
+        borderWidth: 1,
+        borderColor: '#bfbfbf',
+        borderRadius: 5,
+        padding: 10,
+        height: 40
+    },
+    confirmButton: {
+        marginTop: 320,
+        backgroundColor: '#8926D8',
+        padding: 15,
+        borderRadius: 25,
+        alignItems: 'center'
+    },
+    cancelButton: {
+        marginTop: 20,
+        backgroundColor: '#D3483F',
+        padding: 15,
+        borderRadius: 25,
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontFamily: 'Ubuntu_500Medium'
+    }
+
+});
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -18,12 +18,24 @@ const navigationOptions = {
     }
 }
 
+const stackedPageOptions = {
+    title: "Financial Control",
+    headerTitleStyle: { 
+        marginRight: 55,
+        alignSelf: 'center',
+    },
+    headerTintColor: "#8926D8",
+    headerStyle: {
+        backgroundColor: "white",
+    }
+}
+
 const Routes = () => {
     return (
         <NavigationContainer>
             <AppStack.Navigator>
                 <AppStack.Screen name="Main" component={Main} options={navigationOptions} />
-                <AppStack.Screen name="Create" component={Create} options={navigationOptions} />
+                <AppStack.Screen name="Create" component={Create} options={stackedPageOptions} />
             </AppStack.Navigator>
         </NavigationContainer>
     )
