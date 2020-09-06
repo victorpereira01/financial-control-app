ca4f9df43dbe952b74638fa41387613d617e5bb9
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-06 00:07:17 -0300

    feat: getting data from api

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import api from '../../services/api';
 
 export default class Main extends Component {
+
+    state = {
+        user: [],
+        transactions: [],
+    }
+
+    componentDidMount() {
+        this.loadData();
+        this.loadTransactions();
+    }
+
+    loadData = async () => {
+        const response = await api.get('/1');
+
+        const user = response.data;
+
+        this.setState({ user });
+    }
+
+    loadTransactions = async () => {
+        const response = await api.get('/1/transactions');
+
+        const transactions = response.data;
+
+        this.setState({ transactions });
+    }
+
     render() {
         return (
             <View>
-                <Text>Main</Text>
+                <Text>{this.state.user.balance}</Text>
+                <Text>{this.state.user.revenue}</Text>
+                <Text>{this.state.user.expenses}</Text>
+                {this.state.transactions.map(transaction => (
+                    <Text key={transaction.id}>{transaction.name} {transaction.value}</Text>
+                ))}
             </View>
         )
     }
---
+++ mobile/src/services/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://financial-contr01.herokuapp.com/users'
+})
+
+export default api;
