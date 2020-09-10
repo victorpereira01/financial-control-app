31edcda2613c308f73dc8c2080c070115ddc3429
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-10 17:19:56 -0300

    feat: login and register pages template

--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -1,9 +1,43 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { TextInput, RectButton } from 'react-native-gesture-handler';
+import { Link } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Login() {
 
     return (
-        <Text>Login Page</Text>
+        <View>
+            <Text>Financial Control</Text>
+
+            <View >
+                <Text>Login</Text>
+
+                <View>
+                    <Text>E-mail</Text>
+                    <TextInput></TextInput>
+                </View>
+
+                <View>
+                    <Text>Password</Text>
+                    <TextInput secureTextEntry={true}></TextInput>
+                </View>
+
+                <Text>Forgot password?</Text>
+
+                <RectButton>
+                    <Text>Sign In</Text>
+                </RectButton>
+
+                <Text>OR</Text>
+
+                <View>
+                    <Icon name="facebook-square"></Icon>
+                    <Icon name="google-plus-square"></Icon>
+                    <Icon name="twitter-square"></Icon>
+                </View>
+
+            </View>
+        </View>
     )
 }
