7a9be0320549c898c5a9ef862eaab8e5ca798bb7
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-10 18:31:38 -0300

    style: login and register pages stylesheet

--- mobile/src/pages/Register/index.js
+++ mobile/src/pages/Register/index.js
@@ -4,38 +4,132 @@ import { TextInput, RectButton } from 'react-native-gesture-handler';
 import { Link } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import globalStyles from '../../../public/stylesheets/main';
+
 export default function Login() {
 
     return (
-        <View>
-            <Text>Financial Control</Text>
+        <View style={styles.container}>
 
-            <View >
-                <Text>Sign Up</Text>
+            <Text style={styles.header}>Financial Control</Text>
 
-                <View>
-                    <Text>E-mail</Text>
-                    <TextInput></TextInput>
+            <View style={styles.form} >
+                <View style={styles.content}>
+
+                    <Text style={styles.title}>Sign Up</Text>
+
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputText}>E-mail</Text>
+                        <TextInput style={styles.input}></TextInput>
+                    </View>
+
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.inputText}>Password</Text>
+                        <TextInput style={styles.input} secureTextEntry={true}></TextInput>
+                    </View>
+
+                    <View style={styles.blank} />
+
+                    <RectButton style={globalStyles.button}>
+                        <Text style={globalStyles.buttonText}>Sign In</Text>
+                    </RectButton>
+
+                    <Text style={styles.or}>OR</Text>
+
+                    <View style={styles.iconsContainer}>
+                        <Icon style={styles.icon} name="facebook-square"></Icon>
+                        <Icon style={styles.icon} name="google-plus-square"></Icon>
+                        <Icon style={styles.icon} name="twitter-square"></Icon>
+                    </View>
+
                 </View>
-
-                <View>
-                    <Text>Password</Text>
-                    <TextInput secureTextEntry={true}></TextInput>
-                </View>
-
-                <RectButton>
-                    <Text>Sign Up</Text>
-                </RectButton>
-
-                <Text>OR</Text>
-
-                <View>
-                    <Icon name="facebook-square"></Icon>
-                    <Icon name="google-plus-square"></Icon>
-                    <Icon name="twitter-square"></Icon>
-                </View>
-
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#8926D8'
+    },
+    header: {
+        color: 'white',
+        fontSize: 40,
+        fontFamily: 'Ubuntu_400Regular',
+        marginRight: 15,
+        marginLeft: 15,
+        paddingTop: 60,
+        paddingBottom: 30
+    },
+    form: {
+        height: '100%',
+        backgroundColor: 'white',
+        borderRadius: 25,
+    },
+    content: {
+        marginRight: 15,
+        marginLeft: 15,
+    },
+    title: {
+        color: '#575757',
+        fontSize: 24,
+        fontFamily: 'Ubuntu_500Medium',
+        paddingTop: 50
+    },
+    inputContainer: {
+        paddingTop: 50
+    },
+    inputText: {
+        color: '#575757',
+        fontSize: 18,
+        fontFamily: 'Ubuntu_500Medium',
+        paddingBottom: 5
+    },
+    input: {
+        fontSize: 17,
+        fontFamily: 'Ubuntu_400Regular',
+        borderWidth: 1,
+        borderColor: '#bfbfbf',
+        borderRadius: 5,
+        padding: 10,
+        height: 40
+    },
+    forgot: {
+        color: '#575757',
+        fontSize: 14,
+        fontFamily: 'Ubuntu_400Regular',
+        alignSelf: 'flex-end',
+        paddingTop: 8,
+        paddingBottom: 40
+    },
+    create: {
+        color: '#575757',
+        fontFamily: 'Ubuntu_400Regular',
+        paddingTop: 30,
+        alignSelf: 'center'
+    },
+    signUp: {
+        color: '#8926D8',
+        fontFamily: 'Ubuntu_500Medium'
+    },
+    or: {
+        color: '#e0e0e0',
+        fontSize: 25,
+        fontFamily: 'Ubuntu_500Medium',
+        padding: 25,
+        alignSelf: 'center'
+    },
+    iconsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },  
+    icon: {
+        paddingRight: 20,
+        paddingLeft: 20,
+        color: '#383838',
+        fontSize: 50
+    },
+    blank: {
+        padding: 40
+    }
+})
