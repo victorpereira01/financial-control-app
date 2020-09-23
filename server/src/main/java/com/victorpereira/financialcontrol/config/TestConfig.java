32d76953f91fe77147a8a0b5242f6081bc83d26f
Victor Pereira <victor.cordeiro2@gmail.com>
2020-09-23 16:49:48 -0300

    fix: hashing test profile passwords

--- server/src/main/java/com/victorpereira/financialcontrol/config/TestConfig.java
+++ server/src/main/java/com/victorpereira/financialcontrol/config/TestConfig.java
@@ -11,14 +11,14 @@ import org.springframework.context.annotation.Profile;
 import com.victorpereira.financialcontrol.models.Transaction;
 import com.victorpereira.financialcontrol.models.User;
 import com.victorpereira.financialcontrol.repositories.TransactionRepository;
-import com.victorpereira.financialcontrol.repositories.UserRepository;
+import com.victorpereira.financialcontrol.services.UserService;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
 
 	@Autowired
-	private UserRepository userRepo;
+	private UserService userService;
 
 	@Autowired
 	private TransactionRepository transactionRepo;
@@ -29,7 +29,7 @@ public class TestConfig {
 		User u1 = new User(null, "victor.cordeiro2@gmail.com", "123", 1200.00, 1500.00, 300.00);
 		User u2 = new User(null, "ana@gmail.com", "123", 200.00, 1100.00, 900.00);
 		User u3 = new User(null, "joao@gmail.com", "123", 650.00, 1650.00, 1000.00);
-		
+
 		Transaction t1 = new Transaction(null, "Salário", 450.00);
 		Transaction t2 = new Transaction(null, "Conserto do carro", 100.00);
 		Transaction t3 = new Transaction(null, "Festa", 55.00);
@@ -37,13 +37,15 @@ public class TestConfig {
 		t1.setUser(u1);
 		t2.setUser(u1);
 		t3.setUser(u2);
-		
+
 		u1.setTransactions(Arrays.asList(t1, t2));
 		u2.setTransactions(Arrays.asList(t3));
 
-		userRepo.saveAll(Arrays.asList(u1, u2, u3));
+		userService.insert(u1);
+		userService.insert(u2);
+		userService.insert(u3);
 		transactionRepo.saveAll(Arrays.asList(t1, t2, t3));
-		
+
 		return true;
 	}
 }
